9831c970317242ad6be3de38f78c8d856c1388af
csuzum <>
2021-04-14 15:48:30 -0700

    initial commit

--- lesson01/assignment/scripts.js
+++ lesson01/assignment/scripts.js
@@ -1,33 +1,150 @@
-// Code that requires the DOM be loaded should not be run until the DOM is loaded
-document. addEventListener("DOMContentLoaded", function(){
+// Requires the DOM be loaded should not be run until the DOM is loaded
+document.addEventListener("DOMContentLoaded", function(){
 
-  // Find the example form, and handle its submit event
-  const exampleForm = document.querySelector('#example-form')
-
-  exampleForm.addEventListener('submit', function(event){
-    // Prevent the form from trying to submit to a server
+  // Find the the form, and handle its submit event
+  const myForm1 = document.querySelector('#myForm1')
+  
+  myForm1.addEventListener('submit', function(event){
+  // Prevent the form from trying to submit to a server
     event.preventDefault()
 
-    // An array that we can transform based on user input
-    let data = [0, 1, 2, 3, 5, 8]
-
-    const inputField = document.querySelector('#example-form .input')
+    // An array to be transformed based on user input
+    const array1 = [5, 10, 15, 20, 25]
+    
+    const inputField = document.querySelector('#myForm1 .input')
     const userInput = inputField.value
     const userNumber = parseInt(userInput)
 
-    // If the user's input was a number, push it into the array and continue
-    if(!isNaN(userNumber)){
-      data.push(userNumber)
+    // Use to multiple all array items by userInput number
+    const newArray1 = array1.map(x => x * userNumber);
 
-      // Use reduce to sum all the numbers in the data array
-      const sum = data.reduce((accumulator, currentElement) => accumulator + currentElement);
+    // Output the new array
+    document.querySelector('#myForm1 .output').innerHTML = newArray1
 
-      // Output the total to the page- no need to convert the numeric sum back to a string, HTML will just print the number as a string
-      document.querySelector('#example-form .output').innerHTML = sum
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+})
+
+// Requires the DOM be loaded should not be run until the DOM is loaded
+document.addEventListener("DOMContentLoaded", function(){
+
+  // Find the the form, and handle its submit event
+  const myForm2 = document.querySelector('#myForm2')
+  
+  myForm2.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array to be accessed with user input
+    const pizzaMenu = ['Cheese', 'Pepperoni', 'Vegetarian', 'Hawaiian', 'Margherita', 'Supreme', 'Mushroom']
+    
+    const inputField = document.querySelector('#myForm2 .input')
+    const userInput = inputField.value
+    // Use to compare user input to array items
+    const pizzaOrder = (pizzaMenu.includes(userInput));
+    
+    // If user input is found in the array, output this
+    if(pizzaOrder === true) {
+      document.querySelector('#myForm2 .output').innerHTML = 'Your order will be ready shortly.';
+    // If user input is not found in the array, output this
+    } else {
+      document.querySelector('#myForm2 .output').innerHTML = 'Not available. Please choose again.';
     }
 
     // Clear the form field so the user can try again
     inputField.value = ''
   })
+})
 
+// Requires the DOM be loaded should not be run until the DOM is loaded
+document. addEventListener("DOMContentLoaded", function(){
+  const myForm3 = document.querySelector('#myForm3')
+
+  // Find the the form, and handle its submit event
+  myForm3.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array to be accessed with user input
+    let myClass = [18, 21, 42, 65, 99]
+
+    const inputField = document.querySelector('#myForm3 .input')
+    const userInput = inputField.value
+    const userNumber = parseInt(userInput)
+
+    if(!isNaN(userNumber)){
+      // Use to compare user if user input is smaller than array items
+      let youngest = myClass.every( e => e > userNumber)
+
+      // Output true if user input is smaller than any array item, false if input is the same or larger than any array item  
+      document.querySelector('#myForm3 .output').innerHTML = youngest
+    }
+
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+})
+
+// Requires the DOM be loaded should not be run until the DOM is loaded
+document. addEventListener("DOMContentLoaded", function(){
+  const myForm3 = document.querySelector('#myForm4')
+
+  // Find the the form, and handle its submit event
+  myForm4.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+    // An array to be accessed with user input
+    let myClass = [18, 21, 34, 42, 65, 99]
+
+    const inputField = document.querySelector('#myForm4 .input')
+    const userInput = inputField.value
+    const userNumber = parseInt(userInput)
+  
+    if(!isNaN(userNumber)){
+      // Use to compare user input to array items
+      let older = myClass.filter( e => e > userNumber)
+
+      // output all array items larger than user input
+      document.querySelector('#myForm4 .output').innerHTML = older
+    }
+
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
+})
+
+// Requires the DOM be loaded should not be run until the DOM is loaded
+document.addEventListener("DOMContentLoaded", function(){
+
+  // Find the the form, and handle its submit event
+  const myForm3 = document.querySelector('#myForm5')
+  
+  myForm5.addEventListener('submit', function(event){
+    // Prevent the form from trying to submit to a server
+    event.preventDefault()
+    
+    // An array to be accessed with user input
+    const kittenInventory = [
+      {breed: 'Tabby', quantity: 25},
+      {breed: 'Calico', quantity: 9},
+      {breed: 'Siamese', quantity: 2},
+      {breed: 'Sphynx', quantity: 3},
+      {breed: 'Bengal', quantity: 0},
+      {breed: 'Manx', quantity: 5},
+      {breed: 'Persian', quantity: 1}
+    ];
+
+    const inputField = document.querySelector('#myForm5 .input')
+    const userInput = inputField.value
+    // Use to compare user input to array items
+    const kittensAvailable = kittenInventory.find(({breed}) => breed === userInput);
+    
+    // output the array item quantity based on user input 
+    document.querySelector('#myForm5 .output').innerHTML = kittensAvailable.quantity
+    
+    // Clear the form field so the user can try again
+    inputField.value = ''
+  })
 })
