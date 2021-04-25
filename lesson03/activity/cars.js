2338490ff3702f12696aa9eefd4fac279e226f09
csuzum <>
2021-04-25 16:03:50 -0700

    lesson3 & start of assignment

--- lesson03/activity/cars.js
+++ lesson03/activity/cars.js
@@ -1,3 +1,13 @@
 // TODO: Create HTML code from cars JSON
 
 const carsJSON = '[{"make":"Ford","model":"Mustang","year":2010,"color":"black"},{"make":"Chevy","model":"Corvette","year":1984,"color":"red"},{"make":"Jeep","model":"Wrangler","year":1999,"color":"silver"}]'
+
+function Car(color, make, model, year) {
+  this.color = color;
+  this.make = make;
+  this.model = model;
+  this.year = year;
+}
+
+const myCar = new Car('Black', 'Chevy', 'Cruze Hatch', 2018);
+console.log(myCar);
