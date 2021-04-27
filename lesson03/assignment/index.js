7f4cd72f42f594df1d3c730304310d2ada5956a1
csuzum <>
2021-04-26 18:28:57 -0700

    initial commit

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -1,4 +1,4 @@
-[
+const employees = [
   {
     "name": "Paolo Maldini",
     "jobTitle": "Front End Developer",
@@ -48,7 +48,32 @@
     "email": "maribel@example.com",
     "linkedInUrl": "maribel.linkedinprofile.com",
     "codeLanguages": [
-       "HTML", "CSS", "JavaScript", "React", "Vue", "Redux"
+      "HTML", "CSS", "JavaScript", "React", "Vue", "Redux"
     ]
-  }
- ]
+  },
+];
+
+const container = document.querySelector('#main-container')
+
+employees.forEach(function(el) {
+  let list = document.createElement('article')
+  list.setAttribute('class', 'employee-card')
+  list.innerHTML = ` 
+    <div class='name-container'>
+      <img class='circle-img' src='./img/headshot.jpg' alt='face of beagle wearing a black bow tie'>
+      <h1>${el.name}</h1>
+      <h2>${el.jobTitle}</h2>
+    </div>
+    <div class='profile-detail'>
+      <ul>
+        <li><span>Company: </span>${el.company}</li>
+        <li><span>Experience: </span>${el.experience}</li>
+        <li><span>School: </span>${el.school}</li>
+        <li><span>Major: </span>${el.major}</li>
+        <li><span>Email: </span>${el.email}</li>
+        <li><span>LinkedIn URL: </span>${el.linkedInUrl}</li>
+        <li><span>Code Languages: </span>${el.codeLanguages.join(',')}</li>
+    </div>
+  `
+  container.append(list)
+})
