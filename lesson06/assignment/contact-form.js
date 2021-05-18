fb509bc177cf325f706ad5fb5e1ff881e7a3ed70
csuzum <>
2021-05-17 17:22:15 -0700

    initial commit

---
+++ lesson06/assignment/contact-form.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+  // Find the the form, and handle its submit event
+  const form = document.querySelector('#contactForm')
+
+  form.addEventListener('submit', function(event){
+  // Prevent the form from trying to submit to a server
+    event.preventDefault()
+
+  var serializeArray = function (form) {
+    var arr = [];
+    Array.prototype.slice.call(form.elements).forEach(function (field) {
+      if (!field.name || field.disabled || ['file', 'reset', 'submit', 'button'].indexOf(field.type) > -1) return;
+      if (field.type === 'select-multiple') {
+        Array.prototype.slice.call(field.options).forEach(function (option) {
+          if (!option.selected) return;
+          arr.push({
+            name: field.name,
+            value: option.value
+          });
+        });
+        return;
+      }
+      if (['checkbox', 'radio'].indexOf(field.type) >-1 && !field.checked) return;
+      arr.push({
+        name: field.name,
+        value: field.value
+      });
+
+
+    });
+    return(arr);
+  };
+
+  var form = document.querySelector('#contactForm');
+  var data = serializeArray(form);
+  console.log(data);
+
+  })
+})
